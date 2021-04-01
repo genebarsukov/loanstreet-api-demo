09a6b4b19b0009e73c704d8e227f39cb2858442a
Gene <gene@Genes-MBP.fios-router.home>
2021-04-01 13:22:31 -0400

    update postgres config

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
     'type': "postgres",
     'url': process.env.DATABASE_URL || 'postgresql://gene@localhost/loanstreet-demo',
+    'ssl': true,
     'synchronize': false,
     'entities': ['dist/**/*.entity{.ts,.js}']
 }
